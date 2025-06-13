7df0b7815537710d1321749bb01550f62bcc112f
苏木 <sumu@example.com>
2025-06-13 16:33:58 +0800

    添加项目代码格式化工具及配置文件

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,25 @@
  * File name  : index.ts
  * Author     : 苏木
  * Date       : 2025-06-13
- * Version    : 
- * Description: 
+ * Version    :
+ * Description:
  * ======================================================
  */
 
-const message: string = "Hello, World!";
+function testPrettier(input: number): string {
+  try {
+    if (input > 0) {
+      return 'Positive';
+    } else if (input < 0) {
+      throw new Error('Negative');
+    }
+    return 'Zero';
+  } catch (err) {
+    console.error(err);
+    return 'Error';
+  }
+}
+
+const message: string = 'Hello, World!';
 console.log(message);
+console.log(testPrettier(5));
