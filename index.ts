ec76aaf70bb2c1bbde304dd9cf755c20cadca8eb
苏木 <sumu@example.com>
2025-06-13 10:42:05 +0800

    添加一个HelloWorld示例

---
+++ index.ts
@@ -0,0 +1,12 @@
+/** =====================================================
+ * Copyright © hk. 2022-2025. All rights reserved.
+ * File name  : index.ts
+ * Author     : 苏木
+ * Date       : 2025-06-13
+ * Version    : 
+ * Description: 
+ * ======================================================
+ */
+
+const message: string = "Hello, World!";
+console.log(message);
